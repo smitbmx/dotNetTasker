ab6f469b0ba6d5fa77e87cf803f9a81ca9dabcb8
smitbmx <smitbmx@gmail.com>
2014-10-13 22:20:25 +0300

    Added class with inheritance with different types (nothing special here)

---
+++ OOP/Inheritance3DifferentTypes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Inheritance3DifferentTypes
+    {
+        public interface IInterface
+        {
+            void PrintName();
+        }
+
+        public abstract class MyAbstractClass
+        {
+            private void PrintName()
+            {
+                Console.WriteLine("I am an abstract class");
+            }
+        }
+
+        public class MyClass : IInterface
+        {
+            public void PrintName()
+            {
+                Console.WriteLine("I am a class");
+            }
+        }
+
+        public struct MyStruct : IInterface
+        {
+            public void PrintName()
+            {
+                Console.WriteLine("I am a struct");
+            }
+        }
+
+        public void Start()
+        {
+        }
+    }
+}
