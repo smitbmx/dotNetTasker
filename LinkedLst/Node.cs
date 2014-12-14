ec277e6c7a96114372c39cf1935802045df1ce43
smitbmx <smitbmx@gmail.com>
2014-12-14 22:36:11 +0200

    Implemented Node class in LinkedList project

--- LinkedLst/Node.cs
+++ LinkedLst/Node.cs
@@ -20,5 +20,25 @@ namespace LinkedLst
          * [ ] next - get/set the next field
          */
 
+        private object data;
+        private Node next;
+
+        public Node(object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public object Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
     }
 }
