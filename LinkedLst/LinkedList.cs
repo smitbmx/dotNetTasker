003393dfaaa0dfc2c5f01ae07accbbc75477eeaa
smitbmx <smitbmx@gmail.com>
2014-12-14 22:35:43 +0200

    Added project Linked List

---
+++ LinkedLst/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLst
+{
+    public class LinkedList
+    {
+        /* Constructor:
+         * [ ] LinkedList() - Initializes the private fields
+         * 
+         * Private Fields:
+         * [ ] Node head - Is a reference to the FIRST node in the list
+         * [ ] int count - The current size of the list
+         * 
+         * Public Properties:
+         * [ ] Empty - If the list is empty
+         * [ ] Count - How many items are in the list
+         * [ ] Indexer - Access data like an array.
+         * 
+         * Methods:
+         * [ ] Add(int index, object o) - Add an item to list at specified index
+         * [ ] Add(object o) - Add an item to the END of the list
+         * [ ] Remove(int index) - Remove the item in the list at the specified index
+         * [ ] Clear() - Clear the list
+         * [ ] IndexOf(int object o) - gets the index of the item in the list, if not in the list return -1
+         * [ ] Contains(object o) - if the list contains the object
+         * [ ] Get(int index) - Gets item at the specified index 
+         */
+    }
+}
---
+++ LinkedLst/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLst
+{
+    public class Node
+    {
+        /* Constructor:
+         * [ ] Node(object data, Node next)
+         * 
+         * Private Fields:
+         * [ ] object data - contain the data of the node, what we want to store in the list
+         * [ ] Node next - reference to the next node in the list
+         * 
+         * Public Properties:
+         * [ ] Data - get/set the data field
+         * [ ] next - get/set the next field
+         */
+
+    }
+}
